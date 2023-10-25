be4aa3a44cef438ac0b7d41f9d95db5d4cafc1d5
Aurelien <aurelien.emeriau@hotmail.fr>
2023-10-25 15:12:06 +0200

    ajout logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import Background from "./components/Background";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div>
+      <Logo />
       <Background />
     </div>
   );
