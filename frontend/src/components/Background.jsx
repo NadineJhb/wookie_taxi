092ce16f57130255d8a19b1426f28089c9902cd8
Aurelien <aurelien.emeriau@hotmail.fr>
2023-10-25 14:12:47 +0200

    ajout fichier background

---
+++ frontend/src/components/Background.jsx
@@ -0,0 +1,32 @@
+function Background() {
+  return (
+    <>
+      <h1>LE COVOIT' DE LA GALAXIE !</h1>
+      <div className="Alderaan">
+        <img src="src/public/images/planets/Alderaan.png" alt="Alderaan" />
+      </div>
+
+      <div className="Dagobah">
+        <img src="src/public/images/planets/Dagobah.png" alt="Dagobah" />
+      </div>
+
+      <div className="DeathStar">
+        <img src="src/public/images/planets/DeathStar.png" alt="DeathStar" />
+      </div>
+
+      <div className="Mustafar">
+        <img src="src/public/images/planets/Mustafar.png" alt="Mustafar" />
+      </div>
+
+      <div className="Tatooin">
+        <img src="src/public/images/planets/Tatooin.png" alt="Tatooin" />
+      </div>
+
+      <div className="YavinIV">
+        <img src="src/public/images/planets/Yavin IV.png" alt="Yavin IV" />
+      </div>
+    </>
+  );
+}
+
+export default Background;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style/index.scss";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
