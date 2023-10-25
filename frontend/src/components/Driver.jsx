ef2fff38931360f3ec922eebf5a80499f6c971a1
Nadine <nadine.motel@gmail.com>
2023-10-25 16:33:48 +0200

    ajout du composant driver

--- frontend/src/components/Driver.jsx
+++ frontend/src/components/Driver.jsx
@@ -3,11 +3,11 @@ import "../style/_driver.scss";
 export default function Driver() {
   return (
     <div className="card">
-      <div className="imgDiv">
+      <div className="driverImgDiv">
         <img
           src="src/public/images/characters/yoda.jpg"
           alt="Avatar"
-          className="w-100"
+          className="driverImg"
         />
       </div>
       <div className="info-container">
@@ -30,6 +30,11 @@ export default function Driver() {
       </div>
       <div className="right-side">
         <div className="isFavorite" />
+        <img
+          src="src/public/images/starship/kana.png"
+          alt="kana"
+          className="starshipImg"
+        />
         <button type="button">Réserver</button>
       </div>
     </div>
