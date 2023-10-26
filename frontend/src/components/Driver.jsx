6b973e0eb2abcf71dbb5a9a23191882555d0ad7d
Nadine <nadine.motel@gmail.com>
2023-10-26 15:19:35 +0200

    test

--- frontend/src/components/Driver.jsx
+++ frontend/src/components/Driver.jsx
@@ -1,42 +1,54 @@
 import "../style/_driver.scss";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Driver() {
-  return (
-    <div className="card">
-      <div className="driverImgDiv">
-        <img
-          src="src/public/images/characters/yoda.jpg"
-          alt="Avatar"
-          className="driverImg"
-        />
+  const [people, setPeople] = useState([]);
+
+  axios.get("https://swapi.dev/api/people").then((res) => {
+    setPeople(res.data.results);
+  });
+
+  return people.map((person) => {
+    return (
+      <div className="card">
+        <div className="driverIdBlock">
+          <div className="driverImgDiv">
+            <img
+              src="src/public/images/characters/yoda.jpg"
+              alt="Avatar"
+              className="driverImg"
+            />
+          </div>
+          <div className="info-container">
+            <h2>{person.name}</h2>
+            <p>
+              <strong>Birth year :</strong> {person.birth_year}
+            </p>
+            <p>
+              <strong>Height :</strong> {person.height} cm
+            </p>
+            <p>
+              <strong>Gender :</strong> {person.gender}
+            </p>
+            <p>
+              <strong>Skin :</strong> {person.skin_color}
+            </p>
+            <p>
+              <strong>Eyes :</strong> {person.eye_color}
+            </p>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="isFavorite" />
+          <img
+            src="src/public/images/starship/kana.png"
+            alt="kana"
+            className="starshipImg"
+          />
+          <button type="button">Réserver</button>
+        </div>
       </div>
-      <div className="info-container">
-        <h2>Maître Yoda</h2>
-        <p>
-          <strong>Espèce :</strong> Inconnue
-        </p>
-        <p>
-          <strong>Origine :</strong> Dagobah
-        </p>
-        <p>
-          <strong>Véhicule :</strong> Snowspeeder bike
-        </p>
-        <p>
-          <strong>Catégorie :</strong> Speeder
-        </p>
-        <p>
-          <strong>Passagers :</strong> 1
-        </p>
-      </div>
-      <div className="right-side">
-        <div className="isFavorite" />
-        <img
-          src="src/public/images/starship/kana.png"
-          alt="kana"
-          className="starshipImg"
-        />
-        <button type="button">Réserver</button>
-      </div>
-    </div>
-  );
+    );
+  });
 }
