23ff3ed9a2b3447a593b7015265cd93dad53893c
FUKUI AKANE <akane.2702@gmail.com>
2023-10-31 13:20:26 +0100

    passed prettier

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -5,20 +5,23 @@ import { BiSolidMap } from "react-icons/bi";
 /* recuperer depart, destination passangers + search bar */
 function SearchBar() {
   return (
-    <div className="container">
-      <div className="search-from">
-        <BiSolidMap className="map-icon" /> Départ : ENDOR
-      </div>
+    <div className="title-searchbar-container">
+      <h1>LE COVOIT' DE LA GALAXIE !</h1>
+      <div className="searchbar-container">
+        <div className="search-from">
+          <BiSolidMap className="map-icon" /> Départ : ENDOR
+        </div>
 
-      <form action="">
-        <input type="text" placeholder="Destination" className="search-to" />
-        <input type="text" placeholder="1" className="search-number" />
-        <button type="submit" className="search-btn">
-          <div>
-            <FiSearch className="search-icon" />
-          </div>
-        </button>
-      </form>
+        <form action="">
+          <input type="text" placeholder="Destination" className="search-to" />
+          <input type="text" placeholder="1" className="search-number" />
+          <button type="submit" className="search-btn">
+            <div>
+              <FiSearch className="search-icon" />
+            </div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
