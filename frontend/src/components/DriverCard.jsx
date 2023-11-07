05654f62456e0fadbc76a75e0924bba7b8b373e5
Nadine <nadine.motel@gmail.com>
2023-11-07 14:01:14 +0100

    affichage des drivers avec véhicule

--- frontend/src/components/DriverCard.jsx
+++ frontend/src/components/DriverCard.jsx
@@ -1,63 +1,60 @@
 import PropTypes from "prop-types";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function DriverCard({
-  driverName,
-  birthYear,
-  height,
-  gender,
-  skinColor,
-  eyeColor,
-}) {
+function DriverCard({ driverName, driverVehicleUrl }) {
+  const [vehicle, setVehicle] = useState(undefined);
+
+  useEffect(() => {
+    if (driverVehicleUrl) {
+      axios.get(driverVehicleUrl).then((res) => {
+        setVehicle(res.data);
+      });
+    }
+  }, []);
+
   return (
-    <div className="card">
-      <div className="driverIdBlock">
-        <div className="driverImgDiv">
-          <img
-            src={`src/public/images/characters/${driverName}.jpg`}
-            alt="Avatar"
-            className="driverImg"
-          />
+    vehicle && (
+      <div className="card">
+        <div className="driverIdBlock">
+          <div className="driverImgDiv">
+            <img
+              src={`src/public/images/characters/${driverName}.jpg`}
+              alt="Avatar"
+              className="driverImg"
+            />
+          </div>
+          <div className="info-container">
+            <h2>{driverName}</h2>
+            <p>
+              <strong>Vehicle name</strong> {vehicle.name}
+            </p>
+            <p>
+              <strong>Vehicle model :</strong> {vehicle.model}
+            </p>
+            <p>
+              <strong>Passengers :</strong> {vehicle.passengers}
+            </p>
+          </div>
         </div>
-        <div className="info-container">
-          <h2>{driverName}</h2>
-          <p>
-            <strong>Birth year :</strong> {birthYear}
-          </p>
-          <p>
-            <strong>Height :</strong> {height}cm
-          </p>
-          <p>
-            <strong>Gender :</strong> {gender}
-          </p>
-          <p>
-            <strong>Skin :</strong> {skinColor}
-          </p>
-          <p>
-            <strong>Eyes :</strong> {eyeColor}
-          </p>
-        </div>
-      </div>
-      <div className="right-side">
-        <div className="isFavorite">
-          <img
-            src="src/public/images/starship/kana.png"
-            alt="kana"
-            className="starshipImg"
-          />
-          <button type="button">Réserver</button>
+        <div className="right-side">
+          <div className="isFavorite">
+            <img
+              src="src/public/images/starship/kana.png"
+              alt="kana"
+              className="starshipImg"
+            />
+            <button type="button">Réserver</button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
 DriverCard.propTypes = {
   driverName: PropTypes.string.isRequired,
-  birthYear: PropTypes.string.isRequired,
-  gender: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired,
-  skinColor: PropTypes.string.isRequired,
-  eyeColor: PropTypes.string.isRequired,
+  driverVehicleUrl: PropTypes.shape.isRequired,
 };
 
 export default DriverCard;
